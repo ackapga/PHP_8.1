f6640d381fbecf130a9d9adf21248ebcf2be5373
Ackap <ackapga@gmail.com>
2022-10-30 03:00:20 +0600

    2. Подправил, убрал значения по-умолчанию.

--- src/Blog/Comment.php
+++ src/Blog/Comment.php
@@ -5,9 +5,9 @@ namespace Ackapga\Habrahabr\Blog;
 class Comment
 {
     public function __construct(
-        private ?UUID  $uuid = null,
-        private ?UUID  $post_uuid = null,
-        private ?UUID  $author_uuid = null,
+        private UUID  $uuid,
+        private UUID  $post_uuid,
+        private UUID  $author_uuid,
         private string $text,
     )
     {
@@ -19,49 +19,49 @@ class Comment
     }
 
     /**
-     * @return UUID|null
+     * @return UUID
      */
-    public function getUuid(): ?UUID
+    public function getUuid(): UUID
     {
         return $this->uuid;
     }
 
     /**
-     * @param UUID|null $uuid
+     * @param UUID $uuid
      */
-    public function setUuid(?UUID $uuid): void
+    public function setUuid(UUID $uuid): void
     {
         $this->uuid = $uuid;
     }
 
     /**
-     * @return UUID|null
+     * @return UUID
      */
-    public function getPostUuid(): ?UUID
+    public function getPostUuid(): UUID
     {
         return $this->post_uuid;
     }
 
     /**
-     * @param UUID|null $post_uuid
+     * @param UUID $post_uuid
      */
-    public function setPostUuid(?UUID $post_uuid): void
+    public function setPostUuid(UUID $post_uuid): void
     {
         $this->post_uuid = $post_uuid;
     }
 
     /**
-     * @return UUID|null
+     * @return UUID
      */
-    public function getAuthorUuid(): ?UUID
+    public function getAuthorUuid(): UUID
     {
         return $this->author_uuid;
     }
 
     /**
-     * @param UUID|null $author_uuid
+     * @param UUID $author_uuid
      */
-    public function setAuthorUuid(?UUID $author_uuid): void
+    public function setAuthorUuid(UUID $author_uuid): void
     {
         $this->author_uuid = $author_uuid;
     }
