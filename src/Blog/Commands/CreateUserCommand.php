69fcb405a684125198186935c70c55167adf3a9a
Ackap <ackapga@gmail.com>
2022-10-30 00:20:03 +0600

    2. Доработал Скрипты UUID User.

---
+++ src/Blog/Commands/CreateUserCommand.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Ackapga\Habrahabr\Blog\Commands;
+
+
+use Ackapga\Habrahabr\Blog\Exceptions\CommandException;
+use Ackapga\Habrahabr\Blog\Exceptions\InvalidArgumentException;
+use Ackapga\Habrahabr\Blog\Exceptions\UserNotFoundException;
+use Ackapga\Habrahabr\Blog\Exceptions\ArgumentsException;
+use Ackapga\Habrahabr\Blog\Interfaces\UsersRepositoryInterface;
+use Ackapga\Habrahabr\Blog\UUID;
+use Ackapga\Habrahabr\Person\Name;
+use Ackapga\Habrahabr\Person\User;
+
+final class CreateUserCommand
+{
+    public function __construct(
+        private UsersRepositoryInterface $usersRepository
+    )
+    {
+    }
+
+    /**
+     * @param Arguments $arguments
+     * @return void
+     * @throws CommandException
+     * @throws InvalidArgumentException
+     * @throws ArgumentsException
+     */
+    public function handle(Arguments $arguments): void
+    {
+        $username = $arguments->get('username');
+        if ($this->userExists($username)) {
+            throw new CommandException("Пользователь $username уже существует в БД!");
+        }
+        $this->usersRepository->save(new User(
+            UUID::random(),
+            $username,
+            new Name($arguments->get('first_name'), $arguments->get('last_name'))
+        ));
+    }
+
+    /**
+     * @param string $username
+     * @return bool
+     */
+    private function userExists(string $username): bool
+    {
+        try {
+            $this->usersRepository->getByUsername($username);
+        } catch (UserNotFoundException) {
+            return false;
+        }
+        return true;
+    }
+
+}
---
+++ src/Person/Name.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Ackapga\Habrahabr\Person;
+
+class Name
+{
+    public function __construct(
+        private string $firstName,
+        private string $lastName
+    )
+    {
+    }
+
+    public function __toString(): string
+    {
+        return $this->firstName . ' ' . $this->lastName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFirstName(): string
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @param string $firstName
+     */
+    public function setFirstName(string $firstName): void
+    {
+        $this->firstName = $firstName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLastName(): string
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * @param string $lastName
+     */
+    public function setLastName(string $lastName): void
+    {
+        $this->lastName = $lastName;
+    }
+
+}
