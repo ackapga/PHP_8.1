35677d74dd8e2a07fd8a3f17b161932d0d31922f
Ackap <ackapga@gmail.com>
2022-11-14 21:06:41 +0600

    Поменял имя Класса.

--- src/Interfaces/LikeRepositoryInterface.php
+++ src/Interfaces/LikeRepositoryInterface.php
@@ -2,11 +2,11 @@
 
 namespace Ackapga\Habrahabr\Interfaces;
 
-use Ackapga\Habrahabr\Blog\Like;
+use Ackapga\Habrahabr\Blog\PostLike;
 use Ackapga\Habrahabr\Blog\UUID;
 
 interface LikeRepositoryInterface
 {
     public function getByPostUuid(UUID $uuid): array;
-    public function save(Like $like): void;
+    public function save(PostLike $like): void;
 }
